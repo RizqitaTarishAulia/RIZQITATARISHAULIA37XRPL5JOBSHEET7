98bd30168ae03b914f8b425a718b66ab98469e0d
Tarishaulia <Tarishaulia@DESKTOP-88IEU4V>
2018-08-01 09:44:38 +0700

    Initial Commit

---
+++ src/Latihan/CobaPercabangan.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+
+/**
+ *
+ * @author Tarishaulia
+ */
+public class CobaPercabangan {
+    public static void main(String[]args){
+        String identitas = "RIZQITA TARISH AULIA / XRPL5 / 37";
+        System.out.println("Identitas : "+ identitas);
+}
+}
+

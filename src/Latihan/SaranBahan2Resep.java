e62ac9fd073be659b5233d10bf7d79f972ee7d3e
Tarishaulia <Tarishaulia@DESKTOP-88IEU4V>
2018-08-03 17:17:05 +0700

    Initial Commit

---
+++ src/Latihan/SaranBahan2Resep.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+
+/**
+ *
+ * @author Tarishaulia
+ */
+public class SaranBahan2Resep {
+    public static void main(String[]args){
+        String identitas = "RIZQITA TARISH AULIA / XRPL5 / 37";
+        System.out.println("Identitas : " +identitas);
+        
+        System.out.print("\nSaran Resep dari Bahan Milik Anda\n");
+        
+    }
+}

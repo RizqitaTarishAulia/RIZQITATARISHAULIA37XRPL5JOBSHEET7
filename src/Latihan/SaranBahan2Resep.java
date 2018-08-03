7449b6cd62318a6a220309502946d7df4c635bb7
Tarishaulia <Tarishaulia@DESKTOP-88IEU4V>
2018-08-03 17:21:05 +0700

    Tambah Input Bahan 1

--- src/Latihan/SaranBahan2Resep.java
+++ src/Latihan/SaranBahan2Resep.java
@@ -5,6 +5,8 @@
  */
 package Latihan;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Tarishaulia
@@ -15,6 +17,14 @@ public class SaranBahan2Resep {
         System.out.println("Identitas : " +identitas);
         
         System.out.print("\nSaran Resep dari Bahan Milik Anda\n");
+       
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Bahan pertama");
+        System.out.println("1. Pisang");
+        System.out.println("2. Telur");
+        System.out.print("Masukkan no pilihan Anda : ");
+        int bahan1 = scanner.nextInt();
         
     }
 }
